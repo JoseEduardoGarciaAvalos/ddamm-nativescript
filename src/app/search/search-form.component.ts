009f7f9322d1ab65dc873880e8e2908ec529b9e4
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-11-04 18:05:54 -0600

    Primer commit

---
+++ src/app/search/search-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+
+@Component({
+  selector: 'ns-search-form',
+  templateUrl: './search-form.component.html'
+})
+export class SearchFormComponent implements OnInit{
+  textFliedValue: string ="";
+  @Output() search: EventEmitter<string> = new EventEmitter();
+  @Input() inicial : string;
+  onButtonTap(): void{
+    console.dir(this.textFliedValue);
+    //if(this.textFliedValue.length>2){
+      this.search.emit(this.textFliedValue);
+    //}
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+    //this.textFliedValue = this.inicial;
+  }
+
+}
