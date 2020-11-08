12b316045a7091435c6a65680f86ad239d88a193
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-11-08 11:49:44 -0600

    2.3 'pull to refresh' en ListView desde un servicio

--- src/app/_Busqueda/detalle/detalle.component.ts
+++ src/app/_Busqueda/detalle/detalle.component.ts
@@ -18,16 +18,29 @@ export class DetalleComponent implements OnInit {
         this.route.queryParams.subscribe( params =>{
             this.cuerpo = params["valor"]
         })
-        let can = this.detalleS.buscar().length + 1;
-        const LIM = can + 10;
-        for(let i=can; i<LIM; i++){
-            this.detalleS.agregar("detalle "+i);
-        }
+        this.agregarElementos();
     }
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    refrescar(args) {
+        const pullRefresh = args.object; //Documentación en => https://github.com/nstudio/nativescript-pulltorefresh
+        const componente = this;
+        setTimeout(function () {
+            componente.agregarElementos();
+            pullRefresh.refreshing = false;
+        }, 1000);
+    }
+
+    agregarElementos() {
+        let can = this.detalleS.buscar().length + 1;
+        const LIM = can + 10;
+        for (let i = can; i < LIM; i++) {
+            this.detalleS.agregar("detalle " + i);
+        }
     }
 
 }
