3b7b78742e6e1bb47110ea47c87dd860cb48a3f7
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-11-08 11:35:46 -0600

    preparación para implementar plugin 'pull to refresh'

--- src/app/_Busqueda/detalle/detalle.component.ts
+++ src/app/_Busqueda/detalle/detalle.component.ts
@@ -2,20 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
 import * as app from "tns-core-modules/application";
+import { DetalleService } from '../detalle.service';
 
 @Component({
     selector: 'ns-detalle',
     templateUrl: './detalle.component.html',
+    providers: [DetalleService] 
 })
 export class DetalleComponent implements OnInit {
     public cuerpo:string = "Titulo del detalle";
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(public detalleS : DetalleService, private route: ActivatedRoute) { }
 
     ngOnInit() {
         this.route.queryParams.subscribe( params =>{
             this.cuerpo = params["valor"]
         })
+        let can = this.detalleS.buscar().length + 1;
+        const LIM = can + 10;
+        for(let i=can; i<LIM; i++){
+            this.detalleS.agregar("detalle "+i);
+        }
     }
 
     onDrawerButtonTap(): void {
