87da2d07b8b9a384ffee73b1f1995a84b70838fe
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-11-16 15:27:03 -0600

    Implementación de REDUX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,22 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NoticiasService } from "./_Mi_Modulo/noticias.service";
 
+import { ActionReducerMap, StoreModule as NgRxStoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { initializeNoticiasState, NoticiasEffects, NoticiasState, reducersNoticias } from "./_Mi_Modulo/noticias-state.models";
+
+export interface AppState {
+    noticias: NoticiasState;
+}
+
+const reducers: ActionReducerMap<AppState> = {
+    noticias: reducersNoticias
+};
+
+const reducersInitialState = {
+    noticias: initializeNoticiasState()
+};
+
 @NgModule({
     bootstrap: [
         AppComponent
@@ -13,7 +29,9 @@ import { NoticiasService } from "./_Mi_Modulo/noticias.service";
     imports: [
         AppRoutingModule,
         NativeScriptModule,
-        NativeScriptUISideDrawerModule
+        NativeScriptUISideDrawerModule,
+        NgRxStoreModule.forRoot(reducers, {initialState: reducersInitialState}),
+        EffectsModule.forRoot([NoticiasEffects])
     ],
     declarations: [
         AppComponent,
