775d6486c9da4ce3b5b4f60df76d27e2bd9d293a
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-11-16 16:13:11 -0600

    3.10 Lista de favoritos en la pantalla principal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+import { AppState } from "../app.module";
+import { Noticia } from "../_Mi_Modulo/noticias-state.models";
 
 @Component({
     selector: "Home",
@@ -8,12 +11,16 @@ import * as app from "tns-core-modules/application";
 })
 export class HomeComponent implements OnInit {
 
-    constructor() {
-        // Use the component constructor to inject providers.
+    public favoritos: Array<Noticia> = [];
+
+    constructor(private store: Store<AppState>) {
+        // Use the component constructor to inject providers. 
     }
 
     ngOnInit(): void {
-        // Init your component properties here.
+        this.store.select((state) => state.noticias.favoritos).subscribe((data) => {
+            this.favoritos = data
+        });
     }
 
     onDrawerButtonTap(): void {
