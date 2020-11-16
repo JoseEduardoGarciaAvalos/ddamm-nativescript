32888a0993f0211abbb76ee3aa068634ae176c28
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-11-16 16:00:13 -0600

    3.8-9 Lista de favoritos y botón que despache un action

--- src/app/featured/featured.component.ts
+++ src/app/featured/featured.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+import { AppState } from "../app.module";
+import { FavoritoAction, Noticia } from "../_Mi_Modulo/noticias-state.models";
 
 @Component({
     selector: "Featured",
@@ -8,16 +11,26 @@ import * as app from "tns-core-modules/application";
 })
 export class FeaturedComponent implements OnInit {
 
-    constructor() {
+    public favoritos: Array<Noticia> = [];
+
+    constructor( private store: Store<AppState>) {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
-        // Init your component properties here.
+        this.store.select((state) => state.noticias.favoritos).subscribe((data) => {
+            //console.log("DATA", JSON.stringify(data))
+            this.favoritos = data
+        });
     }
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    leerAhora(){
+        //console.log("Noticia genérica")
+        this.store.dispatch(new FavoritoAction(new Noticia("Noticia genérica")));
     }
 }
