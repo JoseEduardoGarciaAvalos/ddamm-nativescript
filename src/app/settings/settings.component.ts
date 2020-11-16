4954dc565cd2515399fdcc5e7846e4351db15c37
José Eduardo García Avalos <joseeduardogarciaavalos@hotmail.es>
2020-11-16 12:32:44 -0600

    3.5-6 LocalStorage mostrar usuario y editarlo en formulario

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
-
+let localStorage = require("nativescript-localstorage");
 @Component({
     selector: "Settings",
     templateUrl: "./settings.component.html"
 })
 export class SettingsComponent implements OnInit {
+    public usuario: string;
+    public mensaje: string;
 
     constructor() {
-        // Use the component constructor to inject providers.
+        this.usuario = localStorage.getItem("usuario");
+        if (this.usuario == null) this.mensaje = "Usuario no autenticado"
+        else this.mensaje = "Bienvenido " + this.usuario
+        console.log(localStorage.getItem("usuario"));
     }
 
     ngOnInit(): void {
@@ -19,5 +24,14 @@ export class SettingsComponent implements OnInit {
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    guardar(): void {
+        console.log(this.usuario);
+        if (this.usuario == "" || this.usuario == null) this.mensaje = "Usuario no autenticado"
+        else {
+            this.mensaje = "Bienvenido " + this.usuario
+            localStorage.setItem("usuario", this.usuario);
+        }
     }
 }
